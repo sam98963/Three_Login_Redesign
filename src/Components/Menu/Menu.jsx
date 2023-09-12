01dad847d55caadc6850951b1b788432889b2e33
sam98963 <szwilliamson01@gmail.com>
2023-09-12 17:11:46 +0100

    input styling

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -13,16 +13,16 @@ export default function Menu({menuOpen, setMenuOpen}){
   return(<>
     {menuOpen ? (
         
-    <aside className="h-full w-full sm:w-96 lg:w-112 bg-primary absolute top-0 right-0 flex flex-col items-end text-2xl">
-      <div className= "h-60 w-full bg-secondary flex flex-col items-end justify-center" onClick={handleMenuClose}>
-        <div className="flex row-reverse justify-end w-1/2 py-2 border-t-1 border-tertiary border-dashed mr-5">
-          <h2 className="text-5xl font-bold">Welcome!</h2>
+    <aside className="h-full w-full sm:w-96 lg:w-112 bg-primary absolute top-0 right-0 flex flex-col items-end text-3xl overflow-hidden">
+      <div className= "sm:h-60 h-52 w-full bg-secondary flex flex-col items-end justify-center" onClick={handleMenuClose}>
+        <div className="flex row-reverse justify-end w-1/2 pb-5 border-t-1 border-tertiary border-dashed mr-5">
+          <h2 className="text-5xl font-bold pt-5">Welcome!</h2>
         </div>
-        <div className="flex row-reverse justify-end items-center w-1/2 py-2 border-t-1 border-tertiary border-dashed mr-5">
+        <div className="flex row-reverse justify-end items-center w-1/2 pb-2 border-t-1 border-tertiary border-dashed mr-5">
           <p className="mr-2">Sign In</p>
           <img src = {SignIn} className="h-6 w-6 inline-block -mt-1"/>
         </div>
-        <div className="flex row-reverse justify-end items-center w-1/2 py-2 border-t-1 border-tertiary border-dashed mr-5">
+        <div className="flex row-reverse justify-end items-center w-1/2 pb-2 border-t-1 border-tertiary border-dashed mr-5">
           <p className="mr-2">Register</p>
           <img src = {Register} className="h-6 w-6 inline-block -mt-1"/>
         </div>
@@ -30,7 +30,7 @@ export default function Menu({menuOpen, setMenuOpen}){
       </div>
 
       <div className="py-5 flex items-center justify-end w-full mr-5">
-        <input type= "text" className="w-57.5 placeholder:text-base py-1 pb-2 pl-2 rounded md:placeholder:text-lg" placeholder="Search"></input>
+        <input type= "text" className="w-57.5 placeholder:text-lg py-2 pl-2 rounded sm:w-2/3 lg:placeholder:text-xl border-tertiary border-1 focus:border-primary focus:outline-tertiary text-xl " placeholder="Search"></input>
       </div>
       <div className="flex row-reverse justify-end items-center w-1/2 py-5 border-t-1 border-tertiary border-dashed mr-5">
         <p className="mr-2">Store</p>
@@ -44,7 +44,7 @@ export default function Menu({menuOpen, setMenuOpen}){
         <p className="mr-2">Blog</p>
         <img src = {BlogIcon} className="h-6 w-6 inline-block"/>
       </div>
-      <h1 className="text-9xl font-bold self-center absolute -bottom-11 ">Three</h1>
+      <h1 className="text-9xl font-bold self-center absolute -bottom-11">Three</h1>
     </aside>
     
     )
