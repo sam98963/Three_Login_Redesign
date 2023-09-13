6c03127c13cac1d1d8090be359b611493254a1ec
sam98963 <szwilliamson01@gmail.com>
2023-09-13 11:27:36 +0100

    menu reshuffle, cursor pointer and spacing

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
   return(
     <div className="md:w-97.5 w-90 md:mx-3 mx-1 h-64 bg-secondary my-4 rounded-lg border-tertiary border-1 md:h-80 flex">
     <div className="text-center self-end w-full overflow-hidden">
-      <h1 className="text-16xl font-bold md:-mb-19 xl:-mb-24 xl:text-20xl hidden md:block">Three</h1>
+      <h1 className="text-16xl font-bold md:-mb-19 xl:-mb-24 xl:text-20xl hidden md:block tracking-wide">Three</h1>
     </div>
   </div>
   )
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -4,6 +4,7 @@ import Question from "../../Assets/Question.png"
 import ShoppingBag from "../../Assets/ShoppingBag.png"
 import Register from "../../Assets/Register.png"
 import SignIn from "../../Assets/SignIn.png"
+import Close from "../../Assets/Close.png"
 export default function Menu({menuOpen, setMenuOpen}){
 
   function handleMenuClose(){
@@ -13,38 +14,54 @@ export default function Menu({menuOpen, setMenuOpen}){
   return(<>
     {menuOpen ? (
         
-    <aside className="h-full w-full sm:w-96 lg:w-112 bg-primary absolute top-0 right-0 flex flex-col items-end text-3xl overflow-hidden">
-      <div className= "sm:h-60 h-52 w-full bg-secondary flex flex-col items-end justify-center" onClick={handleMenuClose}>
-        <div className="flex row-reverse justify-end w-1/2 pb-5 border-t-1 border-tertiary border-dashed mr-5">
+    <aside className="h-full w-full sm:w-96 lg:w-112 bg-primary absolute top-0 right-0 text-3xl overflow-hidden">
+
+      <section className= "sm:h-60 h-52 w-full bg-secondary flex flex-col items-end justify-center">
+
+        <div onClick={handleMenuClose} className="self-start justify-self-end absolute top-0 mt-4 ml-5 cursor-pointer">
+          <img src = {Close}  className="h-10 w-10 inline-block -mt-1"/>
+        </div>
+
+        <div className="flex row-reverse justify-end w-1/2 pb-5 border-t-1 border-tertiary border-dashed mr-5 ">
           <h2 className="text-5xl font-bold pt-5">Welcome!</h2>
         </div>
-        <div className="flex row-reverse justify-end items-center w-1/2 pb-2 border-t-1 border-tertiary border-dashed mr-5">
-          <p className="mr-2">Sign In</p>
+
+        <div className="flex row-reverse justify-end items-center w-1/2 pb-2 border-t-1 border-tertiary border-dashed mr-5 ">
+          <p className="mr-2 cursor-pointer">Sign In</p>
           <img src = {SignIn} className="h-6 w-6 inline-block -mt-1"/>
         </div>
+
         <div className="flex row-reverse justify-end items-center w-1/2 pb-2 border-t-1 border-tertiary border-dashed mr-5">
-          <p className="mr-2">Register</p>
+          <p className="mr-2 cursor-pointer">Register</p>
           <img src = {Register} className="h-6 w-6 inline-block -mt-1"/>
         </div>
-           <p className="self-start justify-self-end">X</p>
-      </div>
 
-      <div className="py-5 flex items-center justify-end w-full mr-5">
-        <input type= "text" className="w-57.5 placeholder:text-lg py-2 pl-2 rounded sm:w-2/3 lg:placeholder:text-xl border-tertiary border-1 focus:border-primary focus:outline-tertiary text-xl " placeholder="Search"></input>
-      </div>
-      <div className="flex row-reverse justify-end items-center w-1/2 py-5 border-t-1 border-tertiary border-dashed mr-5">
-        <p className="mr-2">Store</p>
-        <img src = {ShoppingBag} className="h-6 w-6 inline-block -mt-1"/>
-      </div>
-      <div className="flex row-reverse justify-end items-center w-1/2 py-5 border-t-1 border-tertiary border-dashed mr-5">
-        <p className="mr-2">Help</p>
-        <img src = {Question} className="h-6 w-6 inline-block"/>
-      </div>
-      <div className="flex row-reverse justify-end items-center w-1/2 py-5 border-t-1 border-tertiary border-dashed mr-5">
-        <p className="mr-2">Blog</p>
-        <img src = {BlogIcon} className="h-6 w-6 inline-block"/>
-      </div>
-      <h1 className="text-9xl font-bold self-center absolute -bottom-11">Three</h1>
+        
+      </section>
+
+      <section className="w-full flex flex-col items-end text-3xl">
+
+        <div className="py-5 flex items-center justify-end w-full mr-5">
+          <input type= "text" className="w-57.5 bg-secondary placeholder:text-lg py-2 pl-2 rounded sm:w-2/3 lg:placeholder:text-xl border-tertiary border-1 focus:border-primary focus:outline-tertiary text-xl " placeholder="Search"></input>
+        </div>
+
+        <div className="flex row-reverse justify-end items-center w-1/2 py-5 border-t-1 border-tertiary border-dashed mr-5">
+          <p className="mr-2 cursor-pointer">Store</p>
+          <img src = {ShoppingBag} className="h-6 w-6 inline-block -mt-1"/>
+        </div>
+
+        <div className="flex row-reverse justify-end items-center w-1/2 py-5 border-t-1 border-tertiary border-dashed mr-5">
+          <p className="mr-2 cursor-pointer">Help</p>
+          <img src = {Question} className="h-6 w-6 inline-block"/>
+        </div>
+
+        <div className="flex row-reverse justify-end items-center w-1/2 py-5 border-t-1 border-tertiary border-dashed mr-5">
+          <p className="mr-2 cursor-pointer">Blog</p>
+          <img src = {BlogIcon} className="h-6 w-6 inline-block"/>
+        </div>
+
+        <h1 className="text-9xl font-bold self-center absolute -bottom-11">Three</h1>
+      </section>
     </aside>
     
     )
